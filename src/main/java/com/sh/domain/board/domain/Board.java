75bf8d0a8b55505e614baaeda0ba3c7101d82d9b
songhyeonpk <ehftozl1997@gmail.com>
2023-07-17 17:35:00 +0900

    [#25] feat : 게시글 관련 Entity 설계

---
+++ src/main/java/com/sh/domain/board/domain/Board.java
@@ -0,0 +1,33 @@
+package com.sh.domain.board.domain;
+
+import com.sh.domain.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    // 단방향 매핑
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
