31a0ad56220f11b90b2ae5fa0597c7acfdc4f005
songhyeonpk <ehftozl1997@gmail.com>
2023-07-19 20:04:08 +0900

    [#37] refactor : UserController 코드 refactoring

--- src/main/java/com/sh/domain/user/controller/UserController.java
+++ src/main/java/com/sh/domain/user/controller/UserController.java
@@ -2,13 +2,13 @@ package com.sh.domain.user.controller;
 
 import com.sh.domain.user.dto.*;
 import com.sh.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor    // final 키워드가 붙은 필드에 자동으로 생성자 주입을 해주는 어노테이션
@@ -20,43 +20,41 @@ public class UserController {
     // @Validated : 스프링에서 제공하는 유효성 검증 어노테이션, 그룹별 유효성 검증이 가능, Errors errors로 에러 여부 판단
     // 회원가입
     @PostMapping("/api/v1/users")
-    public ResponseEntity<Long> join(@RequestBody @Valid SignupRequestDto signupRequestDto){
-        return ResponseEntity.created(URI.create("/api/v1/users")).body(userService.join(signupRequestDto));
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long join(@RequestBody @Valid SignupRequestDto signupRequest){
+        return userService.join(signupRequest);
     }
 
     // 로그인
     @PostMapping("/api/v1/users/login")
-    public ResponseEntity<UserLoginResponseDto> login(@RequestBody @Valid UserBasicRequestDto userBasicRequestDto, HttpServletRequest httpServletRequest){
-        return ResponseEntity.ok().body(userService.login(userBasicRequestDto, httpServletRequest));
+    public ResponseEntity<UserLoginResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequest){
+        return ResponseEntity.ok().body(userService.login(loginRequest));
     }
 
     // 내 정보 조회
     @GetMapping("/api/v1/users/me")
-    // controller에서 요청을 수행하기 전 Filter가 요청 헤더에 대한 검증작업 수행(JwtAuthenticationFilter)
-    public ResponseEntity<UserBasicResponseDto> myProfile(@SessionAttribute(name = "userId", required = false) String userId) {
-        return ResponseEntity.ok().body(userService.selectMe(userId));
+    public ResponseEntity<UserBasicResponseDto> myProfile() {
+        return ResponseEntity.ok().body(userService.selectMe());
     }
 
-
     // 회원 삭제
+    @Operation(summary = "회원 삭제", description = "회원을 삭제하는 API입니다. 회원 삭제 시에는 userId가 필요합니다")
     @DeleteMapping("/api/v1/users")
-    public ResponseEntity<UserBasicResponseDto> deleteUser(@RequestBody @Valid UserBasicRequestDto userBasicRequestDto,
-                                                           @SessionAttribute(name = "userId", required = false) String userId) {
-        userService.deleteUser(userBasicRequestDto, userId);
-        return ResponseEntity.noContent().build();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser() {
+        userService.deleteUser();
     }
 
     // 회원 수정(PATCH)
     @PatchMapping("/api/v1/users/me")
-    public ResponseEntity<UserBasicResponseDto> modify(@RequestBody @Valid UpdateUserRequestDto user,
-                                                       @SessionAttribute(name = "userId", required = false) String userId) {
-        userService.modifyMe(user, userId);
-        return ResponseEntity.noContent().build();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void modify(@RequestBody @Valid UpdateUserRequestDto updateRequest) {
+        userService.modifyMe(updateRequest);
     }
 
     // 다른 회원 조회
     @GetMapping("/api/v1/users/{id}")
-    public ResponseEntity<UserBasicResponseDto> selectByOtherUser(@PathVariable("id") Long id) {
+    public ResponseEntity<UserBasicResponseDto> selectByOtherUser(@PathVariable Long id) {
         return ResponseEntity.ok()
                 .body(userService.selectOtherUser(id));
     }
