d9b18b39cb5b80c8a6356f64b643a36b94ce5c89
songhyeonpk <ehftozl1997@gmail.com>
2023-07-20 13:54:30 +0900

    [#37] refactor : Board Controller 코드 수정

--- src/main/java/com/sh/domain/board/controller/BoardController.java
+++ src/main/java/com/sh/domain/board/controller/BoardController.java
@@ -1,16 +1,15 @@
 package com.sh.domain.board.controller;
 
 import com.sh.domain.board.dto.BoardBasicResponseDto;
-import com.sh.domain.board.dto.BoardCreateRequestDto;
+import com.sh.domain.board.dto.CreateBoardRequestDto;
 import com.sh.domain.board.dto.UpdateBoardRequestDto;
 import com.sh.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
-import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,33 +19,29 @@ public class BoardController {
 
     // 게시글 생성
     @PostMapping("/api/v1/board")
-    public ResponseEntity<Long> create(@RequestBody @Valid BoardCreateRequestDto board,
-                                       @SessionAttribute(name = "userId", required = false) String userId) {
-        return ResponseEntity.created(URI.create("/api/v1/board"))
-                .body(boardService.createBoard(board, userId));
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long create(@RequestBody @Valid CreateBoardRequestDto createRequest) {
+        return boardService.createBoard(createRequest);
     }
 
     // 게시글 조회
     @GetMapping("/api/v1/board/{boardId}")
-    public ResponseEntity<BoardBasicResponseDto> selectBoard(@PathVariable("boardId") Long boardId) {
+    public ResponseEntity<BoardBasicResponseDto> selectBoard(@PathVariable Long boardId) {
         return ResponseEntity.ok()
                 .body(boardService.selectBoard(boardId));
     }
 
     // 게시글 수정
     @PutMapping("/api/v1/board/{boardId}")
-    public ResponseEntity<BoardBasicResponseDto> modify(@PathVariable("boardId") Long boardId,
-                                                        @SessionAttribute(name = "userId", required = false) String userId,
-                                                        @RequestBody @Valid UpdateBoardRequestDto afterBoard) {
-        boardService.modifyBoard(boardId, userId, afterBoard);
-        return ResponseEntity.noContent().build();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void modify(@PathVariable Long boardId, @RequestBody @Valid UpdateBoardRequestDto updateRequest) {
+        boardService.modifyBoard(boardId,updateRequest);
     }
 
     // 게시글 삭제
     @DeleteMapping("/api/v1/board/{boardId}")
-    public ResponseEntity<BoardBasicResponseDto> delete(@PathVariable("boardId") Long boardId,
-                                                        @SessionAttribute(name = "userId", required = false) String userId) {
-        boardService.deleteBoard(boardId, userId);
-        return ResponseEntity.noContent().build();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long boardId) {
+        boardService.deleteBoard(boardId);
     }
 }
