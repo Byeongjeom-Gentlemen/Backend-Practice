2ab3ccefbc49c174a9230f4b483a2abdf9311472
songhyeonpk <ehftozl1997@gmail.com>
2023-07-24 14:11:16 +0900

    [#46] refactor : page 관련 error 코드 수정

--- src/main/java/com/sh/global/exception/PageErrorCode.java
+++ src/main/java/com/sh/global/exception/PageErrorCode.java
@@ -3,9 +3,9 @@ package com.sh.global.exception;
 import org.springframework.http.HttpStatus;
 
 public enum PageErrorCode implements ErrorCode {
-    PAGE_VALUE_OVER_RANGE(HttpStatus.BAD_REQUEST, "P_001", "조회할 페이지는 0을 포함한 양수여야 합니다"),
-    SIZE_VALUE_OVER_RANGE(HttpStatus.BAD_REQUEST, "P_002", "한 페이지의 데이터는 최소 1개에서 최대 10개 이여야 합니다"),
-    IS_NOT_INTEGER(HttpStatus.BAD_REQUEST, "P_003", "page값과 size값은 정수만 허용합니다.");
+    PAGE_VALUE_OVER_RANGE(HttpStatus.BAD_REQUEST, "P_001", "조회할 페이지는 양수여야 합니다."),
+    SIZE_VALUE_OVER_RANGE(HttpStatus.BAD_REQUEST, "P_002", "한 페이지의 데이터는 최소 1개에서 최대 10개만을 허용합니다."),
+    IS_NOT_INTEGER(HttpStatus.BAD_REQUEST, "P_003", "페이지와 페이지 크기는 정수만 허용합니다.");
 
     private final HttpStatus status;
     private final String code;
