8f1583ecb9c9a9e9fe2807b14be4603c060bdc80
songhyeonpk <ehftozl1997@gmail.com>
2023-07-24 14:57:21 +0900

    [#48] feat : 댓글을 등록하는 기능 추가

--- src/main/java/com/sh/domain/comment/controller/CommentController.java
+++ src/main/java/com/sh/domain/comment/controller/CommentController.java
@@ -1,9 +1,17 @@
 package com.sh.domain.comment.controller;
 
 import com.sh.domain.comment.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController {
 
     private final CommentService commentService;
+
+    @Operation(summary = "댓글을 등록하는 API", description = "session에 저장되어 있는 id 값과 boardId 값으로 댓글을 등록할 수 있는지 검사하고 content 값으로 댓글을 등록합니다.")
+    @PostMapping("/api/v1/board/{boardId}/comment")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long createComment(@PathVariable Long boardId,
+                              @RequestParam("content") @NotBlank(message = "내용은 필수 입력 값 입니다.") String content) {
+        return commentService.createComment(boardId, content);
+    }
 }
--- src/main/java/com/sh/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sh/domain/comment/repository/CommentRepository.java
@@ -3,4 +3,8 @@ package com.sh.domain.comment.repository;
 import com.sh.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {}
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    // 댓글 등록
+    Comment save(Comment comment);
+}
