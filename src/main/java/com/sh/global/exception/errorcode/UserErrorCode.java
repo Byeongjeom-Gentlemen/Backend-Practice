df75fa745012fc54680863ac5f836192dcbf7c77
songhyeonpk <ehftozl1997@gmail.com>
2023-08-02 18:16:04 +0900

    [#60] refactor : User 관련 Exception 코드 수정

--- src/main/java/com/sh/global/exception/errorcode/UserErrorCode.java
+++ src/main/java/com/sh/global/exception/errorcode/UserErrorCode.java
@@ -12,27 +12,20 @@ public enum UserErrorCode implements ErrorCode {
     // 데이터 중복(아이디, 닉네임)
     ALREADY_EXISTS_ID(HttpStatus.CONFLICT, "U_002", "이미 사용중인 아이디입니다."),
     ALREADY_EXISTS_NICKNAME(HttpStatus.CONFLICT, "U_003", "이미 사용중인 닉네임입니다."),
-    // FORBIDDEN_REQUEST_USER(HttpStatus.FORBIDDEN, "U_018", "해당 리소스에 접근할 권한이 없습니다.(로그인 필요)"),
+    FORBIDDEN_REQUEST_USER(HttpStatus.FORBIDDEN, "U_004", "해당 리소스에 접근할 권한이 없습니다.(로그인 필요)ㄴ"),
 
     // 해당 유저가 없을 경우
-    NOT_FOUND_USER(HttpStatus.UNAUTHORIZED, "U_020", "해당 사용자가 존재하지 않습니다."),
+    NOT_FOUND_USER(HttpStatus.UNAUTHORIZED, "U_005", "해당 사용자가 존재하지 않습니다."),
 
     // 잘못된 인증(해당 아이디가 존재하지 않거나 비밀번호가 틀렸을 경우, 로그인)
-    INVALID_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "U_004", "사용자 인증에 실패하였습니다."),
+    INVALID_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "U_006", "사용자 인증에 실패하였습니다."),
 
     // 비회원일 경우
-    NON_LOGIN(HttpStatus.UNAUTHORIZED, "U_011", "로그인이 필요한 서비스 입니다."),
+    NON_LOGIN(HttpStatus.UNAUTHORIZED, "U_007", "로그인이 필요한 서비스 입니다."),
 
     // 탈퇴한 회원인 경우
-    WITHDRAWN_USER(HttpStatus.UNAUTHORIZED, "U_012", "탈퇴한 회원입니다."),
-
-    /* JWT */
-    NON_TOKEN(HttpStatus.UNAUTHORIZED, "U_008", "토큰정보가 존재하지 않습니다."),
-    WRONG_TYPE_SIGNATURE(HttpStatus.UNAUTHORIZED, "U_005", "잘못된 JWT 서명입니다."),
-    WRONG_TYPE_TOKEN(
-            HttpStatus.UNAUTHORIZED, "U_006", "유효하지 않은 구성의 토큰입니다.(지원되지 않는 형식이나 구성의 토큰입니다.)"),
-    EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "U_007", "만료된 토큰 정보입니다."),
-    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "U_009", "잘못된 토큰 정보입니다.");
+    WITHDRAWN_USER(HttpStatus.UNAUTHORIZED, "U_008", "탈퇴한 회원입니다."),
+    ALREADY_LOGIN(HttpStatus.UNAUTHORIZED, "U_009", "이미 로그인이 되어있습니다.");
 
     private final HttpStatus status;
     private final String code;
