1f10a7b27c2e8b7fee4d11bd1b6736f16275aa38
songhyeonpk <ehftozl1997@gmail.com>
2023-08-07 18:10:11 +0900

    [#62] refactor : Redis에서 사용할 객체로 수정

--- src/main/java/com/sh/domain/user/domain/BlackListToken.java
+++ src/main/java/com/sh/domain/user/domain/BlackListToken.java
@@ -1,24 +1,32 @@
 package com.sh.domain.user.domain;
 
-import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
-@Entity
+import java.util.concurrent.TimeUnit;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "blacklistToken")
 public class BlackListToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String accessToken;
 
-    @Column private String accessToken;
+    private String value;
+
+    @TimeToLive(unit = TimeUnit.MILLISECONDS)
+    private long expiredTime;
 
     @Builder
-    private BlackListToken(String accessToken) {
+    private BlackListToken(String accessToken, long expiredTime) {
         this.accessToken = accessToken;
+        this.value = "logout";
+        this.expiredTime = expiredTime;
     }
 }
