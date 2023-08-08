9d2100b93487fab662282a48caa80b6feceeeac2
songhyeonpk <ehftozl1997@gmail.com>
2023-08-08 18:38:20 +0900

    [#62] feat : Redis 설정 및 RedisTemplate 관련 로직 추가(2가지 방식)

---
+++ src/main/java/com/sh/global/config/RedisConfig.java
@@ -0,0 +1,40 @@
+package com.sh.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableRedisRepositories
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    // RedisTemplate Bean 등록
+    /*@Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        // RedisTemplate를 받아와서 set, get, delete 사용
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }*/
+
+
+}
