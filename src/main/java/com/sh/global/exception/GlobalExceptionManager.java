18c8935eba8a97059635fe40918ad10039deaeb2
songhyeonpk <ehftozl1997@gmail.com>
2023-08-11 17:32:35 +0900

    [#66] feat : 예외처리 방식을 변경함에 따라 handler 로직 수정

--- src/main/java/com/sh/global/exception/GlobalExceptionManager.java
+++ src/main/java/com/sh/global/exception/GlobalExceptionManager.java
@@ -1,15 +1,6 @@
 package com.sh.global.exception;
 
-import com.sh.global.exception.customexcpetion.board.*;
-import com.sh.global.exception.customexcpetion.comment.NotAuthorityException;
-import com.sh.global.exception.customexcpetion.comment.NotFoundCommentException;
-import com.sh.global.exception.customexcpetion.page.PageRangeOverException;
-import com.sh.global.exception.customexcpetion.page.SizeRangeOverException;
-import com.sh.global.exception.customexcpetion.page.ValueIsNotIntegerException;
-import com.sh.global.exception.customexcpetion.token.ExpiredTokenException;
-import com.sh.global.exception.customexcpetion.token.NonTokenException;
-import com.sh.global.exception.customexcpetion.token.UnauthorizedTokenException;
-import com.sh.global.exception.customexcpetion.user.*;
+import com.sh.global.exception.customexcpetion.*;
 import com.sh.global.exception.errorcode.UserErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,158 +20,38 @@ public class GlobalExceptionManager {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
-    // 아이디 중복 예외
-    @ExceptionHandler(AlreadyUsedUserIdException.class)
-    public ResponseEntity<ErrorResponse> existsByIdError(AlreadyUsedUserIdException e) {
+    // User Custom Exception
+    @ExceptionHandler(UserCustomException.class)
+    public ResponseEntity<ErrorResponse> userError(UserCustomException e) {
         final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(response);
     }
 
-    // 닉네임 중복 예외
-    @ExceptionHandler(AlreadyUsedUserNicknameException.class)
-    public ResponseEntity<ErrorResponse> existsByNicknameError(AlreadyUsedUserNicknameException e) {
+    // Board Custom Exception
+    @ExceptionHandler(BoardCustomException.class)
+    public ResponseEntity<ErrorResponse> boardError(BoardCustomException e) {
         final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(response);
     }
 
-    /*@ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponse> badCredentialError(BadCredentialsException e) {
-    	final ErrorResponse response = ErrorResponse.from(UserErrorCode.INVALID_AUTHENTICATION);
-    	return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-    			.body(response);
-    }*/
-
-    // 해당 유저를 찾을 수 없을 때 예외
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponse> userNotFoundError(UserNotFoundException e) {
+    // Comment Custom Exception
+    @ExceptionHandler(CommentCustomException.class)
+    public ResponseEntity<ErrorResponse> commentError(CommentCustomException e) {
         final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(response);
     }
 
-    // 유저의 정보가 일치하지 않을 때 예외
-    @ExceptionHandler(NotMatchesUserException.class)
-    public ResponseEntity<ErrorResponse> notMatchesError(NotMatchesUserException e) {
+    // Token Custom Exception
+    @ExceptionHandler(TokenCustomException.class)
+    public ResponseEntity<ErrorResponse> tokenError(TokenCustomException e) {
         final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(response);
     }
 
-    // 로그인이 되어있지 않을 때 예외
-    @ExceptionHandler(UserNonLoginException.class)
-    public ResponseEntity<ErrorResponse> nonLoginError(UserNonLoginException e) {
+    // Page Custom Exception
+    @ExceptionHandler(PageCustomException.class)
+    public ResponseEntity<ErrorResponse> pageError(PageCustomException e) {
         final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-    }
-
-    // 탈퇴한 회원인 경우
-    @ExceptionHandler(UserWithdrawalException.class)
-    public ResponseEntity<ErrorResponse> userWithdrawError(UserWithdrawalException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-    }
-
-    // 토큰을 발급받지 않아 authentication이 없는 경우
-    @ExceptionHandler(UnauthorizedUserException.class)
-    public ResponseEntity<ErrorResponse> unAuthorizedUserError(UnauthorizedUserException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-    }
-
-    // Request Header에 필요 Token이 없는 경우
-    @ExceptionHandler(NonTokenException.class)
-    public ResponseEntity<ErrorResponse> nonTokenError(NonTokenException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
-    }
-
-    // 만료된 토큰일 경우
-    @ExceptionHandler(ExpiredTokenException.class)
-    public ResponseEntity<ErrorResponse> expiredTokenError(ExpiredTokenException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
-    }
-
-    // 로그아웃을 하지 않고 이미 로그인되어 있는 상태로 재로그인을 요청할 경우
-    @ExceptionHandler(AlreadyLoginException.class)
-    public ResponseEntity<ErrorResponse> alreadyLoginError(AlreadyLoginException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-    }
-
-    // Refresh Token Validate
-    @ExceptionHandler(UnauthorizedTokenException.class)
-    public ResponseEntity<ErrorResponse> unAuthorizedTokenError(UnauthorizedTokenException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
-    }
-
-    // 해당 게시글을 찾을 수 없을 때 예외
-    @ExceptionHandler(NotFoundBoardException.class)
-    public ResponseEntity<ErrorResponse> notFoundBoardError(NotFoundBoardException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-    }
-
-    // 게시글의 작성자 정보가 틀릴 경우 예외
-    @ExceptionHandler(NotMatchesWriterException.class)
-    public ResponseEntity<ErrorResponse> notMatchesWriterError(NotMatchesWriterException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-    }
-
-    // 지원하지 않는 검색종류일 경우(title, writer 외)
-    @ExceptionHandler(UnsupportedSearchTypeException.class)
-    public ResponseEntity<ErrorResponse> unsupportedSearchTypeError(
-            UnsupportedSearchTypeException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    // 등록된 게시글이 없을 경우
-    @ExceptionHandler(BoardListIsEmptyException.class)
-    public ResponseEntity<ErrorResponse> boardListEmptyError(BoardListIsEmptyException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-    }
-
-    // 검색 keyword 값이 null 또는 빈값일 경우
-    @ExceptionHandler(SearchKeywordIsEmptyException.class)
-    public ResponseEntity<ErrorResponse> keywordEmptyError(SearchKeywordIsEmptyException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    // 페이지 범위 에러
-    @ExceptionHandler(PageRangeOverException.class)
-    public ResponseEntity<ErrorResponse> pageRangeOverError(PageRangeOverException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    // 사이즈 범위 에러
-    @ExceptionHandler(SizeRangeOverException.class)
-    public ResponseEntity<ErrorResponse> sizeRangeOverError(SizeRangeOverException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    // 페이지, 사이즈 값이 정수가 아닐 경우
-    @ExceptionHandler(ValueIsNotIntegerException.class)
-    public ResponseEntity<ErrorResponse> valueError(ValueIsNotIntegerException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    // 댓글을 찾을 수 없을 경우
-    @ExceptionHandler(NotFoundCommentException.class)
-    public ResponseEntity<ErrorResponse> notFoundCommentError(NotFoundCommentException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-    }
-
-    // 댓글의 작성자가 로그인한 정보와 다를 경우
-    @ExceptionHandler(NotAuthorityException.class)
-    public ResponseEntity<ErrorResponse> notAuthorityError(NotAuthorityException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(response);
     }
 }
