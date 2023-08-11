54f44e2a28786981f09f8c5ce40ce3104f27146c
songhyeonpk <ehftozl1997@gmail.com>
2023-08-11 17:31:51 +0900

    [#66] feat, refactor : 새로운 ErrorCode 추가 및 기존 ErrorCode 수정

--- src/main/java/com/sh/global/exception/errorcode/BoardErrorCode.java
+++ src/main/java/com/sh/global/exception/errorcode/BoardErrorCode.java
@@ -8,14 +8,13 @@ public enum BoardErrorCode implements ErrorCode {
 
     /* BOARD */
     NOT_FOUND_BOARD(HttpStatus.NOT_FOUND, "B_001", "해당 게시글이 존재하지 않습니다"),
-    EMPTY_BOARD_LIST(HttpStatus.NOT_FOUND, "B_002", "등록된 게시글이 없습니다."),
-    BOARD_NOT_AUTHORITY(HttpStatus.UNAUTHORIZED, "B_003", "해당 게시글에 대한 작업 권한이 존재하지 않습니다."),
-    UNSUPPORTED_SEARCH_TYPE(HttpStatus.BAD_REQUEST, "B_004", "해당 타입의 검색은 지원하지 않습니다."),
-    NOT_FOUND_SEARCH_TITLE(HttpStatus.NOT_FOUND, "B_005", "해당 제목을 포함한 게시글이 존재하지 않습니다."),
-    NOT_FOUND_SEARCH_WRITER(HttpStatus.NOT_FOUND, "B_006", "해당 작성자의 게시글이 존재하지 않습니다."),
-    KEYWORD_EMPTY(HttpStatus.BAD_REQUEST, "B_007", "검색어를 입력해주세요."),
-    DELETED_BOARD(HttpStatus.UNAUTHORIZED, "b_008", "삭제된 게시글입니다."),
-    NOT_FOUND_LIKE(HttpStatus.NOT_FOUND, "B_009", "해당 게시글의 좋아요 기록이 존재하지 않습니다.");
+    BOARD_NOT_AUTHORITY(HttpStatus.UNAUTHORIZED, "B_002", "해당 게시글에 대한 작업 권한이 존재하지 않습니다."),
+    UNSUPPORTED_SEARCH_TYPE(HttpStatus.BAD_REQUEST, "B_003", "해당 타입의 검색은 지원하지 않습니다."),
+    NOT_FOUND_SEARCH_TITLE(HttpStatus.NOT_FOUND, "B_004", "해당 제목을 포함한 게시글이 존재하지 않습니다."),
+    NOT_FOUND_SEARCH_WRITER(HttpStatus.NOT_FOUND, "B_005", "해당 작성자의 게시글이 존재하지 않습니다."),
+    KEYWORD_EMPTY(HttpStatus.BAD_REQUEST, "B_006", "검색어를 입력해주세요."),
+    DELETED_BOARD(HttpStatus.UNAUTHORIZED, "b_007", "삭제된 게시글입니다."),
+    NOT_FOUND_LIKE(HttpStatus.NOT_FOUND, "B_008", "해당 게시글의 좋아요 기록이 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/sh/global/exception/errorcode/PageErrorCode.java
+++ src/main/java/com/sh/global/exception/errorcode/PageErrorCode.java
@@ -3,9 +3,16 @@ package com.sh.global.exception.errorcode;
 import org.springframework.http.HttpStatus;
 
 public enum PageErrorCode implements ErrorCode {
-    PAGE_VALUE_OVER_RANGE(HttpStatus.BAD_REQUEST, "P_001", "조회할 페이지는 양수여야 합니다."),
-    SIZE_VALUE_OVER_RANGE(HttpStatus.BAD_REQUEST, "P_002", "한 페이지의 데이터는 최소 1개에서 최대 10개만을 허용합니다."),
-    IS_NOT_INTEGER(HttpStatus.BAD_REQUEST, "P_003", "페이지와 페이지 크기는 정수만 허용합니다.");
+
+    // pageNumber & pageSize 가 숫자가 아닐 경우
+    IS_NOT_INTEGER(HttpStatus.BAD_REQUEST, "P_001", "PageNumber & PageSize는 정수만 허용합니다."),
+
+    // pageNumber & pageSize 가 음수인 경우
+    NEGATIVE_VALUE(HttpStatus.BAD_REQUEST, "P_002", "PageNumber & PageSize는 음수 값을 허용하지 않습니다."),
+
+    // pageSize 값이 max 값을 벗어난 경우
+    SIZE_RANGE_OVER(HttpStatus.BAD_REQUEST, "P_003", "PageSize의 범위는 0 ~ 10 사이의 정수여야 합니다.");
+
 
     private final HttpStatus status;
     private final String code;
