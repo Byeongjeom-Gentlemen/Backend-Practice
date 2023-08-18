8976888596146102ed728491d18cf1ec51c5fa3a
songhyeonpk <ehftozl1997@gmail.com>
2023-08-18 18:00:20 +0900

    [#73] feat : LikeRepository 메서드 반환 값 변경

--- src/main/java/com/sh/domain/board/repository/LikeRepository.java
+++ src/main/java/com/sh/domain/board/repository/LikeRepository.java
@@ -6,13 +6,15 @@ import com.sh.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
     // 좋아요 체크
     @Query(value = "select b from Like b where b.user = :user and b.board = :board")
     // @Query(value = "select * from likes where user_id = :userId and board_id = :boardId",
     // nativeQuery = true)
-    Like findByUserAndBoard(User user, Board board);
+    Optional<Like> findByUserAndBoard(User user, Board board);
 
     // 좋아요 저장
     Like save(Like like);
