c592a7af3c817424b933b2bc4922b096a1a0b84f
songhyeonpk <ehftozl1997@gmail.com>
2023-08-21 14:14:19 +0900

    [#73] test : 게시글 좋아요 로직 동시성 테스트 추가, Lock을 걸었을 때와 걸지 않았을 때 결과 비교

--- src/test/java/com/sh/BoardApplicationTests.java
+++ src/test/java/com/sh/BoardApplicationTests.java
@@ -1,6 +1,86 @@
 package com.sh;
 
+import com.sh.domain.board.domain.Board;
+import com.sh.domain.board.dto.request.CreateBoardRequestDto;
+import com.sh.domain.board.repository.BoardRepository;
+import com.sh.domain.board.service.BoardService;
+import com.sh.domain.user.domain.User;
+import com.sh.domain.user.dto.request.LoginRequestDto;
+import com.sh.domain.user.dto.request.SignupRequestDto;
+import com.sh.domain.user.repository.UserRepository;
+import com.sh.domain.user.service.AuthService;
+import com.sh.domain.user.service.UserService;
+import com.sh.global.exception.customexcpetion.BoardCustomException;
+import com.sh.global.exception.customexcpetion.UserCustomException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootTest
-class BoardApplicationTests {}
+class BoardApplicationTests {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        SignupRequestDto request = new SignupRequestDto("ehftozl", "thdgus!", "헬로우");
+        userService.join(request);
+
+        User user = userRepository.findById(1L)
+                .orElseThrow(() -> UserCustomException.USER_NOT_FOUND);
+
+        board = Board.builder()
+                .title("헬로우")
+                .content("바이여")
+                .user(user)
+                .build();
+        boardRepository.save(board);
+    }
+
+    @Test
+    void 동시_100명_좋아요_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    // 락을 적용한 경우
+                    boardService.addLikeCountUseRedisson(1L);
+                    // 락을 적용하지 않은 경우
+                    // boardService.addLikeCount(1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Board b = boardRepository.findById(board.getId())
+                .orElseThrow(() -> BoardCustomException.BOARD_NOT_FOUND);
+
+        System.out.println("게시글 좋아요 수 : " + b.getLikeCount());
+        Assertions.assertEquals(b.getLikeCount(), threadCount);
+    }
+}
