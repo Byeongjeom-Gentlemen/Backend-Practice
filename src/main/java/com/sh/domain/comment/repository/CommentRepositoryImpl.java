3943a762998ff865d0a7ca40152089c84c4a761d
songhyeonpk <ehftozl1997@gmail.com>
2023-08-23 14:33:12 +0900

    [#75] feat : querydsl 처리를 위한 CommentRepositoryCustom 추가 및 페이징 댓글 조회 로직 추가

---
+++ src/main/java/com/sh/domain/comment/repository/CommentRepositoryImpl.java
@@ -0,0 +1,72 @@
+package com.sh.domain.comment.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sh.domain.board.domain.Board;
+import com.sh.domain.comment.domain.Comment;
+import com.sh.domain.comment.dto.SimpleCommentResponseDto;
+import com.sh.domain.user.dto.WriterDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+import static com.sh.domain.comment.domain.QComment.comment;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepositoryImpl implements CommentRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Slice<SimpleCommentResponseDto> findByCommentIdLessThanOrderByCreatedAtDesc(Long lastCommentId, Long boardId, Pageable pageable) {
+
+        List<SimpleCommentResponseDto> results = queryFactory
+                .select(Projections.constructor(SimpleCommentResponseDto.class,
+                        comment.commentId,
+                        comment.content,
+                        comment.user,
+                        comment.createdDate,
+                        comment.modifiedDate
+                ))
+                .from(comment)
+                .where(
+                        // no-offset 페이징 처리
+                        ltCommentId(lastCommentId),
+                        comment.board.id.eq(boardId),
+                        comment.delete_at.isNull()
+                )
+                .orderBy(comment.createdDate.desc())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        return checkLastPage(pageable, results);
+    }
+
+    private BooleanExpression ltCommentId(Long lastCommentId) {
+        // lastCommentId == null 이면 처음 page
+        if(lastCommentId == null) {
+            return null;
+        }
+
+        return comment.commentId.lt(lastCommentId);
+    }
+
+    // 무한 스크롤 방식 처리하는 메서드
+    private Slice<SimpleCommentResponseDto> checkLastPage(Pageable pageable, List<SimpleCommentResponseDto> result) {
+        boolean hasNext = false;
+
+        if(result.size() > pageable.getPageSize()) {
+            hasNext = true;
+            result.remove(pageable.getPageSize());
+        }
+
+        return new SliceImpl<>(result, pageable, hasNext);
+    }
+}
