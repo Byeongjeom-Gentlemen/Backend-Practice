b75abafa6e195f3906b44a5136cf0171cdf7cee5
songhyeonpk <ehftozl1997@gmail.com>
2023-08-24 14:37:30 +0900

    [#75] refactor : 사용하지 않는 코드 수정

--- src/main/java/com/sh/domain/board/repository/BoardRepository.java
+++ src/main/java/com/sh/domain/board/repository/BoardRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
 
     // 게시글 생성
     Board save(Board board);
@@ -22,15 +22,4 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     // 게시글 전체 조회
     Page<Board> findAll(Pageable pageable);
-
-    // 제목으로 게시글 검색
-    Page<Board> findByTitleContaining(String keyword, Pageable pageable);
-
-    // 작성자로 게시글 검색
-    Page<Board> findByUserId(Long userId, Pageable pageable);
-
-    // 비관적 락 & 베타 락(읽기, 쓰기 모두 Lock을 검)
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select b from Board b where b.id = :boardId")
-    Optional<Board> findByIdForUpdate(Long boardId);
 }
