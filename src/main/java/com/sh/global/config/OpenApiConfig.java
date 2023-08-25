97d3b45d9488e2ad4a2d58a32f60ef3642268528
songhyeonpk <ehftozl1997@gmail.com>
2023-08-25 13:31:46 +0900

    [#78] refactor : Swagger Security 설정 코드 추가

--- src/main/java/com/sh/global/config/OpenApiConfig.java
+++ src/main/java/com/sh/global/config/OpenApiConfig.java
@@ -1,14 +1,22 @@
 package com.sh.global.config;
 
+import com.sh.global.aop.DisableSwaggerSecurity;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.method.HandlerMethod;
+
+import java.util.Collections;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
     public OpenAPI openAPI() {
         Info info =
@@ -17,6 +25,45 @@ public class OpenApiConfig {
                         .version("v0.0.1")
                         .description("Board Project의 API 명세서입니다.");
 
-        return new OpenAPI().components(new Components()).info(info);
+        // 인증이 필요한 url에 header 추가 (전역 설정)
+        String key = "Access Token (Bearer)";
+        String refreshKey = "Refresh Token";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(key)
+                .addList(refreshKey);
+
+        SecurityScheme accessTokenSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        SecurityScheme refreshTokenSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Refresh");
+
+        Components components = new Components()
+                .addSecuritySchemes(key, accessTokenSecurityScheme)
+                .addSecuritySchemes(refreshKey, refreshTokenSecurityScheme);
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    @Bean
+    public OperationCustomizer customize() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            DisableSwaggerSecurity methodAnnotation =
+                    handlerMethod.getMethodAnnotation(DisableSwaggerSecurity.class);
+            if(methodAnnotation != null) {
+                operation.setSecurity(Collections.emptyList());
+            }
+            return operation;
+        };
     }
 }
