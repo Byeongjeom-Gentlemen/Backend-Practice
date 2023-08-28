416a2092e1927436dd80ed901b3370bbd239d1b9
songhyeonpk <ehftozl1997@gmail.com>
2023-08-28 17:37:35 +0900

    [#80] feat : 로그아웃, 토큰 재발급 로직 변경

--- src/main/java/com/sh/domain/user/service/AuthServiceImpl.java
+++ src/main/java/com/sh/domain/user/service/AuthServiceImpl.java
@@ -11,6 +11,7 @@ import com.sh.global.exception.customexcpetion.UserCustomException;
 import com.sh.global.util.CustomUserDetails;
 import com.sh.global.util.jwt.JwtProvider;
 import com.sh.global.util.jwt.TokenDto;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -61,57 +62,80 @@ public class AuthServiceImpl implements AuthService {
 
     // 로그아웃
     @Override
-    public void logout(String accessToken) {
-        String id = jwtProvider.parseClaims(accessToken).getSubject();
-        RefreshToken token = refreshTokenRedisRepository.findById(id).orElse(null);
+    public void logout(TokenDto token) {
+        // access token null check
+        checkAccessToken(token.getAccessToken());
 
-        // id를 key 값으로 가진 Refresh Token 이 존재하면
-        if (token != null) {
-            // Redis Token 값 삭제
-            userRedisService.deleteRefreshToken(token);
+        // refresh token null check
+        checkRefreshToken(token.getRefreshToken());
 
-            // BlackList Token 저장
-            userRedisService.saveBlackListToken(accessToken);
+        // access token 검증
+        if(jwtProvider.validateToken(token.getAccessToken())) {
+            // access token 에서 회원 아이디 정보를 가져옴.
+            String id = jwtProvider.parseClaims(token.getAccessToken()).getSubject();
+
+            // 회원 아이디로 Refresh Token 정보 조회, 없다면 이미 만료되어 삭제된 상태
+            RefreshToken refreshToken = refreshTokenRedisRepository.findById(id)
+                    .orElseThrow(() -> TokenCustomException.EXPIRED_REFRESH_TOKEN);
+            // 해당 Refresh Token 정보 삭제
+            userRedisService.deleteRefreshToken(refreshToken);
+
+            // access token 의 남은 만료시간 정보를 가져옴.
+            Long expiredTime = jwtProvider.getRemainExpiredTime(token.getAccessToken());
+
+            // 로그아웃 한 access token 을 blacklist token 으로 등록
+            // blacklist token 으로 등록하지 않으면, 서버는 해당 access token 이 로그아웃 처리된 토큰인지 모르고
+            // 아직 유효시간이 만료되지 않은 토큰으로 인식해 오류가 발생하지 않음.
+            // blacklist token 으로 등록함으로써 서버가 로그아웃 된 token 임을 알 수 있도록 하고 잘못된 접근을 방지함.
+            userRedisService.saveBlackListToken(token.getAccessToken(), expiredTime);
         }
     }
 
     // Access Token 재발급
-    // Refresh Token 이 탈취된 상황을 방지해 Access Token 을 함께 요청받아 Access Token 으로 해당 Refresh Token 정보를 조회 및
-    // 비교
-    // 1. Access Token, Refresh Token null check
-    // 2. Access Token 검증, 만료되지 않은 Access Token 일 경우 Exception
-    // 3. Refresh Token 검증, 이미 만료된 Refresh Token 일 경우 Exception
-    // 4. Access Token 값으로 Redis 에 저장된 Refresh Token 정보(userId, Refresh Token, Access Token) 조회, 없을
-    // 경우 Exception
-    // 5. Redis 에서 조회한 Refresh Token 값과 요청으로 넘어온 Refresh Token 값 비교, 다를 경우 Exception
-    // 6. 새로운 Access Token 과 Refresh Token 을 재발급하여 Redis 갱신 및 토큰 정보 반환
+    /**
+     * Refresh Token 이 탈취된 상황을 방지해 Access Token 을 함께 요청받아 Access Token 으로 해당 Refresh Token 정보를 조회 및 비교
+     * 1. Access Token, Refresh Token null check
+     * 2. Access Token 검증, 만료되지 않은 Access Token 일 경우 Exception
+     * 3. Refresh Token 검증, 이미 만료된 Refresh Token 일 경우 Exception
+     * 4. Access Token 값으로 Redis 에 저장된 Refresh Token 정보(userId, Refresh Token, Access Token) 조회, 없을
+     * 경우 Exception
+     * 5. Redis 에서 조회한 Refresh Token 값과 요청으로 넘어온 Refresh Token 값 비교, 다를 경우 Exception
+     * 6. 새로운 Access Token 과 Refresh Token 을 재발급하여 Redis 갱신 및 토큰 정보 반환
+     */
     @Override
-    public TokenDto accessTokenReIssue(String accessToken, String refreshToken) {
-        // Access Token 값 check
-        checkAccessToken(accessToken);
+    public TokenDto accessTokenReIssue(TokenDto token) {
+        // Access Token null check
+        checkAccessToken(token.getAccessToken());
 
-        // Refresh Token 값 check
-        checkRefreshToken(refreshToken);
+        // Refresh Token null check
+        checkRefreshToken(token.getRefreshToken());
 
         // Access Token 값으로 Refresh Token 조회
         // Redis 에 존재하면 아직 유효한 Refresh Token
-        // 존재하지 않으면 Exception (Refresh Token 만료 or Access Token 값 오류)
-        RefreshToken rt = userRedisService.selectRefreshToken(accessToken);
+        // 존재하지 않으면 Exception
+        RefreshToken rt = refreshTokenRedisRepository.findByAccessToken(token.getAccessToken())
+                .orElseThrow(() -> TokenCustomException.EXPIRED_REFRESH_TOKEN);
 
         // Access Token 만료 정보 확인
         // 만약 Access Token 이 만료되지 않은 상태라면, 악의적인 요청으로 간주
-        // 기존에 저장되어 있던 Refresh Token 정보를 삭제하고, Access Token 값을 BlackList Token 으로 등록, 재로그인 요청
-        if (jwtProvider.validateTokenAndIsExpired(accessToken)) {
+        if (jwtProvider.validateTokenAndIsExpired(token.getAccessToken())) {
+            // 기존에 저장되어 있던 Refresh Token 정보 삭제
             userRedisService.deleteRefreshToken(rt);
-            userRedisService.saveBlackListToken(accessToken);
+
+            // Access Token 값을 BlackList Token 으로 등록
+            Long expiredTime = jwtProvider.getRemainExpiredTime(token.getAccessToken());
+            userRedisService.saveBlackListToken(token.getAccessToken(), expiredTime);
+
+            // 재로그인 요청
             throw TokenCustomException.UNAVAILABLE_TOKENS;
         }
 
         // Refresh Token 값 비교
         // 요청으로 넘어온 Refresh Token 값과 Redis 에서 조회한 Refresh Token 값이 다르다면, Access Token 을 탈취당한 경우로 간주
         // 기존에 저장되어 있던 Refresh Token 정보를 삭제하고, 재로그인 요청
-        if (!refreshToken.equals(rt.getRefreshToken())) {
-            userRedisService.saveBlackListToken(accessToken);
+        if (!token.getRefreshToken().equals(rt.getRefreshToken())) {
+            userRedisService.deleteRefreshToken(rt);
+
             throw TokenCustomException.UNAVAILABLE_TOKENS;
         }
 
