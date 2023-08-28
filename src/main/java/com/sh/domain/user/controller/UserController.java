102d1996f998e3861cf6813b7e93d06664b9564f
songhyeonpk <ehftozl1997@gmail.com>
2023-08-28 17:36:09 +0900

    [#80] refactor : 파라미터 값 변경 및 어노테이션 수정

--- src/main/java/com/sh/domain/user/controller/UserController.java
+++ src/main/java/com/sh/domain/user/controller/UserController.java
@@ -6,7 +6,8 @@ import com.sh.domain.user.dto.response.UserBasicResponseDto;
 import com.sh.domain.user.service.AuthService;
 import com.sh.domain.user.service.UserService;
 import com.sh.global.aop.DisableSwaggerSecurity;
-import com.sh.global.aop.TokenInfo;
+import com.sh.global.aop.TokenValueRequired;
+import com.sh.global.util.jwt.TokenDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
@@ -46,24 +47,24 @@ public class UserController {
 
     // 회원 삭제
     @Operation(summary = "회원삭제 API", description = "회원을 삭제하는 API 입니다. 회원삭제 시에는 로그인 여부를 필요로 합니다.")
-    @TokenInfo
+    @TokenValueRequired
     @DeleteMapping("/api/v1/users")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(String accessToken) {
+    public void deleteUser(TokenDto token) {
         userService.deleteUser();
-        authService.logout(accessToken);
+        authService.logout(token);
     }
 
     // 회원 수정(PATCH)
     @Operation(
             summary = "회원수정 API",
             description = "회원정보를 수정하는 API 입니다. 로그인 여부를 필요로 하며, 수정필드 값이 존재할 경우 수정됩니다.")
-    @TokenInfo
+    @TokenValueRequired
     @PatchMapping("/api/v1/users/me")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void modify(@RequestBody @Valid UpdateUserRequestDto updateRequest, String accessToken) {
+    public void modify(@RequestBody @Valid UpdateUserRequestDto updateRequest, TokenDto token) {
         userService.modifyMe(updateRequest);
-        authService.logout(accessToken);
+        authService.logout(token);
     }
 
     // 다른 회원 조회
