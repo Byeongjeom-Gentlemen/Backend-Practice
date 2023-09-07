17957174aa1de322a53eefc0524da25fb7572093
songhyeonpk <ehftozl1997@gmail.com>
2023-09-07 18:17:57 +0900

    [#87] feat : fileUtils class 추가

---
+++ src/main/java/com/sh/global/util/FileUtils.java
@@ -0,0 +1,34 @@
+package com.sh.global.util;
+
+import com.sh.global.exception.customexcpetion.FileCustomException;
+import org.apache.tika.Tika;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class FileUtils {
+
+    private static final Tika tika = new Tika();
+
+    // 파일 확장자 변조 체크
+    public void validImgFile(InputStream inputStream) {
+        try {
+            List<String> notValidTypeList = Arrays.asList("image/jpeg", "image/pjpeg",
+                    "image/png", "image/gif", "image/bmp", "image/x-windows-bmp");
+
+            String mimeType = tika.detect(inputStream);
+            boolean isValid = notValidTypeList.stream()
+                    .anyMatch(notValidType -> notValidType.equalsIgnoreCase(mimeType));
+
+            if(!isValid) {
+                throw FileCustomException.IS_NOT_IMAGE;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
