ef17e1ac2ad66115d1a9a59831f0246a2839b605
songhyeonpk <ehftozl1997@gmail.com>
2023-09-08 16:59:16 +0900

    [#87] refactor : 변수명 수정

--- src/main/java/com/sh/domain/user/domain/UserImage.java
+++ src/main/java/com/sh/domain/user/domain/UserImage.java
@@ -15,30 +15,31 @@ public class UserImage extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
     private Long id;
 
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column
-    private String name;
+    @Column(name = "save_name")
+    private String storeName;
 
-    @Column
+    @Column(name = "original_name")
     private String originalName;
 
-    @Column(nullable = false)
-    private String filePath;
+    @Column(name = "image_path")
+    private String imagePath;
 
     @Builder
-    public UserImage(User user, String name, String originalName, String filePath) {
+    public UserImage(User user, String storeName, String originalName, String imagePath) {
         this.user = user;
-        this.name = name;
+        this.storeName = storeName;
         this.originalName = originalName;
-        this.filePath = filePath;
+        this.imagePath = imagePath;
     }
 
-    public void updatePath(String path) {
-        this.filePath = path;
+    public void updatePath(String imagePath) {
+        this.imagePath = imagePath;
     }
 }
