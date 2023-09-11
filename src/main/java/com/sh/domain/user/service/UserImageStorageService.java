22371d295dbd236887f7fa251a407c1e629f86cc
songhyeonpk <ehftozl1997@gmail.com>
2023-09-11 16:42:37 +0900

    [#87] feat : 회원 이미지 불러오는 로직 추가

--- src/main/java/com/sh/domain/user/service/UserImageStorageService.java
+++ src/main/java/com/sh/domain/user/service/UserImageStorageService.java
@@ -1,12 +1,21 @@
 package com.sh.domain.user.service;
 
 import com.sh.domain.file.dto.FileResponseDto;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 public interface UserImageStorageService {
 
     // 회원 프로필 이미지 업로드
     @Transactional
     void uploadUserImg(MultipartFile file);
+
+    // 회원 프로필 이미지 불러오기
+    @Transactional(readOnly = true)
+    byte[] showUserImg(Long userId);
 }
