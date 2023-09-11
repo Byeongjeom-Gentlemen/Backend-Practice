4569a8e45e6b6e131263a71496aa64d1de92c90b
songhyeonpk <ehftozl1997@gmail.com>
2023-09-11 16:40:03 +0900

    [#87] feat : File 관련 exception 및 errorcode 추가

--- src/main/java/com/sh/global/exception/customexcpetion/FileCustomException.java
+++ src/main/java/com/sh/global/exception/customexcpetion/FileCustomException.java
@@ -8,7 +8,13 @@ public class FileCustomException extends CustomException {
             new FileCustomException(FileErrorCode.IS_NOT_IMAGE);
 
     public static final FileCustomException FILE_DOES_NOT_EXIST =
-            new FileCustomException(FileErrorCode.File_DOES_NOT_EXIST);
+            new FileCustomException(FileErrorCode.FILE_DOES_NOT_EXIST);
+
+    public static final FileCustomException FILE_DOES_NOT_EXIST_REQUEST_VALUE =
+            new FileCustomException(FileErrorCode.FILE_DOES_NOT_EXIST_REQUEST_VALUE);
+
+    public static final FileCustomException FAILED_UPLOAD_FILE =
+            new FileCustomException(FileErrorCode.FAILED_UPLOAD_FILE);
 
     public FileCustomException(FileErrorCode errorCode) {
         super(errorCode);
--- src/main/java/com/sh/global/exception/errorcode/FileErrorCode.java
+++ src/main/java/com/sh/global/exception/errorcode/FileErrorCode.java
@@ -6,9 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum FileErrorCode implements ErrorCode {
 
-    /* BOARD */
     IS_NOT_IMAGE(HttpStatus.BAD_REQUEST, "F_001", "이미지 파일만 업로드 가능합니다."),
-    File_DOES_NOT_EXIST(HttpStatus.BAD_REQUEST, "F_002", "파일정보가 존재하지 않습니다.");
+    FILE_DOES_NOT_EXIST(HttpStatus.NOT_FOUND, "F_002", "파일정보가 존재하지 않습니다."),
+    FILE_DOES_NOT_EXIST_REQUEST_VALUE(HttpStatus.BAD_REQUEST, "F_003", "요청 값에 파일정보가 존재하지 않습니다."),
+    FAILED_UPLOAD_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "F_004", "파일 업로드에 실패하였습니다.");
 
     private final HttpStatus status;
     private final String code;
