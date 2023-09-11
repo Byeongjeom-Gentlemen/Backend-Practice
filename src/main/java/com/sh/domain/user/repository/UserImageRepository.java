cfb174af955be1588a2147939d426ee677ddbd7d
songhyeonpk <ehftozl1997@gmail.com>
2023-09-11 16:42:12 +0900

    [#87] feat : User 정보로 해당 User의 이미지 정보를 찾는 메소드 추가

--- src/main/java/com/sh/domain/user/repository/UserImageRepository.java
+++ src/main/java/com/sh/domain/user/repository/UserImageRepository.java
@@ -1,7 +1,12 @@
 package com.sh.domain.user.repository;
 
+import com.sh.domain.user.domain.User;
 import com.sh.domain.user.domain.UserImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserImageRepository extends JpaRepository<UserImage, Long> {
+    Optional<UserImage> findByUser(User user);
 }
