ac621429e81af48ad868791358720d7c84443de2
songhyeonpk <ehftozl1997@gmail.com>
2023-09-11 16:42:37 +0900

    [#87] feat : 회원 이미지 불러오는 로직 추가

--- src/main/java/com/sh/domain/user/service/UserImageStorageServiceImpl.java
+++ src/main/java/com/sh/domain/user/service/UserImageStorageServiceImpl.java
@@ -9,12 +9,22 @@ import com.sh.domain.user.repository.UserRepository;
 import com.sh.global.exception.customexcpetion.FileCustomException;
 import com.sh.global.exception.customexcpetion.UserCustomException;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
 
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
@@ -32,7 +42,7 @@ public class UserImageStorageServiceImpl implements UserImageStorageService {
     @Override
     public void uploadUserImg(MultipartFile file) {
         if(file.isEmpty()) {
-            throw new RuntimeException();
+            throw FileCustomException.FILE_DOES_NOT_EXIST_REQUEST_VALUE;
         }
 
         User user = userService.getLoginUser();
@@ -40,7 +50,7 @@ public class UserImageStorageServiceImpl implements UserImageStorageService {
         // 이미지 업로드
         FileResponseDto saveFile = filesStorageService.uploadImg(uploadPath, file);
         if(saveFile == null) {
-            throw new RuntimeException();
+            throw FileCustomException.FAILED_UPLOAD_FILE;
         }
 
         UserImage userImage = UserImage.builder()
@@ -53,5 +63,32 @@ public class UserImageStorageServiceImpl implements UserImageStorageService {
         // 이미지 수정
         user.updateImage(userImage);
         imageRepository.save(userImage);
+    }
+
+    // 회원 프로필 이미지 불러오기
+    @Override
+    public byte[] showUserImg(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserCustomException.USER_NOT_FOUND);
+
+        // 기본 이미지 경로
+        String imagePath = uploadPath + File.separator + "basic_profile_img.jpg";
+
+        UserImage image = imageRepository.findByUser(user).orElse(null);
+        // 이미지 정보가 존재한다면 해당 이미지 정보 path 저장
+        // 존재하지 않으면 기본 이미지 정보 저장
+        if(image != null) {
+            imagePath = image.getImagePath();
+        }
+
+        byte[] result = null;
+
+        try(InputStream imageStream = new FileInputStream(imagePath)) {
+            result = IOUtils.toByteArray(imageStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
