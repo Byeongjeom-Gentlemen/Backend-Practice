dcbb493ce5870f986d0ae55962c77fd137888087
songhyeonpk <ehftozl1997@gmail.com>
2023-09-13 18:07:08 +0900

    [#91] add : fileType, fileSize 필드 추가 및 정적 메서드 매개변수 추가

--- src/main/java/com/sh/global/util/file/FileResponseDto.java
+++ src/main/java/com/sh/global/util/file/FileResponseDto.java
@@ -1,4 +1,4 @@
-package com.sh.domain.file.dto;
+package com.sh.global.util.file;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -10,13 +10,17 @@ public class FileResponseDto {
     private String storeFileName;
     private String originalFileName;
     private String filePath;
+    private String fileType;
+    private long fileSize;
 
     public static FileResponseDto of(
-            String storeFileName, String originalFileName, String filePath) {
+            String storeFileName, String originalFileName, String filePath, String fileType, long fileSize) {
         return FileResponseDto.builder()
                 .storeFileName(storeFileName)
                 .originalFileName(originalFileName)
                 .filePath(filePath)
+                .fileType(fileType)
+                .fileSize(fileSize)
                 .build();
     }
 }
