9e579e79912ae8e99f4227be1479bfb72fb01e59
songhyeonpk <ehftozl1997@gmail.com>
2023-09-13 18:09:39 +0900

    [#91] add : 게시글 첨부파일 엔티티 생성

---
+++ src/main/java/com/sh/domain/board/domain/BoardAttachedFile.java
@@ -0,0 +1,49 @@
+package com.sh.domain.board.domain;
+
+import com.sh.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardAttachedFile extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(name = "save_name")
+    private String storeFileName;
+
+    @Column(name = "original_name")
+    private String originalFileName;
+
+    @Column(name = "file_type")
+    private String fileType;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "file_size")
+    private long fileSize;
+
+    @Builder
+    private BoardAttachedFile(Board board, String storeFileName, String originalFileName, String fileType, String filePath, long fileSize) {
+        this.board = board;
+        this.storeFileName = storeFileName;
+        this.originalFileName = originalFileName;
+        this.fileType = fileType;
+        this.filePath = filePath;
+        this.fileSize = fileSize;
+    }
+}
