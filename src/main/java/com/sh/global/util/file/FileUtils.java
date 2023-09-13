f3548658d22d35070297d1c6c740d1a42fcb9380
songhyeonpk <ehftozl1997@gmail.com>
2023-09-13 15:34:09 +0900

    [#91] remove, chore : domain file 패키지 삭제, FileResponseDto 클래스 이동

---
+++ src/main/java/com/sh/global/util/file/FileUtils.java
@@ -0,0 +1,93 @@
+package com.sh.global.util;
+
+import com.sh.domain.file.dto.FileResponseDto;
+import com.sh.global.exception.customexcpetion.FileCustomException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.tika.Tika;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUtils {
+
+    private static final Tika tika = new Tika();
+
+    // 파일 확장자 변조 체크
+    public void validImgFile(InputStream inputStream) {
+        try {
+            List<String> notValidTypeList =
+                    Arrays.asList(
+                            "image/jpeg",
+                            "image/pjpeg",
+                            "image/png",
+                            "image/gif",
+                            "image/bmp",
+                            "image/x-windows-bmp");
+
+            String mimeType = tika.detect(inputStream);
+            boolean isValid =
+                    notValidTypeList.stream()
+                            .anyMatch(notValidType -> notValidType.equalsIgnoreCase(mimeType));
+
+            if (!isValid) {
+                throw FileCustomException.IS_NOT_IMAGE;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 단일 파일 업로드
+    public FileResponseDto uploadFile(String uploadPath, MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+            // 서버에 저장할 파일 이름 설정
+            // 랜덤 고유번호로 파일명을 변경해 저장하여 파일명 중복을 방지함. (파일 덮어쓰기 방지)
+            String storeFileName = UUID.randomUUID() + "." + extractExt(file.getOriginalFilename());
+            // 이미지 파일 저장 경로
+            String saveFilePath = uploadPath + storeFileName;
+
+            // 파일 업로드
+            file.transferTo(Paths.get(saveFilePath));
+
+            return FileResponseDto.of(storeFileName, file.getOriginalFilename(), saveFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // 다중 파일 업로드
+    public List<FileResponseDto> uploadFiles(String uploadPath, MultipartFile[] files) {
+        List<FileResponseDto> list = new ArrayList<>();
+        for(MultipartFile file : files) {
+            if(file.isEmpty()) {
+                continue;
+            }
+            list.add(uploadFile(uploadPath, file));
+        }
+
+        return list;
+    }
+
+    // 확장자 추출
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+    
+    // 파일 삭제
+    public void deleteFile(String uploadPath) {
+        File file = new File(uploadPath);
+        file.delete();
+    }
+}
