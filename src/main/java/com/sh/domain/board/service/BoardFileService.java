e370e6a9f2abb344af090293dca44c758d4cf1b1
songhyeonpk <ehftozl1997@gmail.com>
2023-09-14 13:15:31 +0900

    [#91] add, feat : 게시글 관련 파일 처리를 담당하는 service layer 추가 및 게시글 첨부파일 업로드 로직 생성

---
+++ src/main/java/com/sh/domain/board/service/BoardFileService.java
@@ -0,0 +1,13 @@
+package com.sh.domain.board.service;
+
+import com.sh.domain.board.domain.Board;
+import com.sh.domain.board.domain.BoardAttachedFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface BoardFileService {
+
+    // 게시글 첨부파일 업로드
+    List<BoardAttachedFile> uploadAttachedFiles(Board board, List<MultipartFile> files);
+}
