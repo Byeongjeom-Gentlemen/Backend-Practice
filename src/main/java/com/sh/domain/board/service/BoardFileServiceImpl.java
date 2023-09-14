34da1bac844d7fadd3cd27f72d08180b191e86ae
songhyeonpk <ehftozl1997@gmail.com>
2023-09-14 13:15:31 +0900

    [#91] add, feat : 게시글 관련 파일 처리를 담당하는 service layer 추가 및 게시글 첨부파일 업로드 로직 생성

---
+++ src/main/java/com/sh/domain/board/service/BoardFileServiceImpl.java
@@ -0,0 +1,45 @@
+package com.sh.domain.board.service;
+
+import com.sh.domain.board.domain.Board;
+import com.sh.domain.board.domain.BoardAttachedFile;
+import com.sh.global.util.file.FileResponseDto;
+import com.sh.global.util.file.FileUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.PrePersist;
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class BoardFileServiceImpl implements BoardFileService {
+
+    @Value("${custom.boardFile-upload-path}")
+    private String uploadPath;
+
+    private final FileUtils fileUtils;
+
+    @Override
+    public List<BoardAttachedFile> uploadAttachedFiles(Board board, List<MultipartFile> files) {
+        // 실제 경로에 파일 업로드
+        List<FileResponseDto> fileList = fileUtils.uploadFiles(uploadPath, files);
+
+        // 업로드한 파일 정보를 BoardAttachedFile 형으로 변환
+        List<BoardAttachedFile> attachedFiles = fileList.stream()
+                .map(attachedFile -> BoardAttachedFile.builder()
+                        .board(board)
+                        .storeFileName(attachedFile.getStoreFileName())
+                        .originalFileName(attachedFile.getOriginalFileName())
+                        .fileType(attachedFile.getFileType())
+                        .filePath(attachedFile.getFilePath())
+                        .fileSize(attachedFile.getFileSize())
+                        .build())
+                .collect(Collectors.toList());
+
+        return attachedFiles;
+    }
+}
