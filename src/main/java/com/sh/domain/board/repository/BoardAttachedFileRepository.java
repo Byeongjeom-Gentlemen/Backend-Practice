496e0a193fdbce244d51a8a336b8605d7dc01fc3
songhyeonpk <ehftozl1997@gmail.com>
2023-09-15 17:48:44 +0900

    [#93] feat : 게시글 엔티티와 저장된 파일명으로 첨부파일 엔티티를 조회하는 로직 추가

--- src/main/java/com/sh/domain/board/repository/BoardAttachedFileRepository.java
+++ src/main/java/com/sh/domain/board/repository/BoardAttachedFileRepository.java
@@ -3,10 +3,15 @@ package com.sh.domain.board.repository;
 import com.sh.domain.board.domain.Board;
 import com.sh.domain.board.domain.BoardAttachedFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardAttachedFileRepository extends JpaRepository<BoardAttachedFile, Long> {
 
     List<BoardAttachedFile> findAllByBoard(Board board);
+
+    @Query(value = "select b from BoardAttachedFile b where b.board = :board and b.storeFileName = :storeFileName")
+    Optional<BoardAttachedFile> findByBoardAndStoreFileName(Board board, String storeFileName);
 }
