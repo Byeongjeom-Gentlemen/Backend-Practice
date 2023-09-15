cf80363a8fb4f99278ce1a6e321b9a9ae4db6fd7
songhyeonpk <ehftozl1997@gmail.com>
2023-09-15 17:47:54 +0900

    [#93] add : FileResponseDto에 필드 추가

--- src/main/java/com/sh/domain/board/dto/response/BoardFileResponseDto.java
+++ src/main/java/com/sh/domain/board/dto/response/BoardFileResponseDto.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 @Getter
 public class BoardFileResponseDto {
 
+    private String storeName;
     private String originalName;
     private String filePath;
     private String fileType;
 
-    public static BoardFileResponseDto of(String originalName, String filePath, String fileType) {
+    public static BoardFileResponseDto of(String storeName, String originalName, String filePath, String fileType) {
         return BoardFileResponseDto.builder()
+                .storeName(storeName)
                 .originalName(originalName)
                 .filePath(filePath)
                 .fileType(fileType)
