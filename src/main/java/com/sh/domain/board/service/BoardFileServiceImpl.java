28a3fe112259b95516c597a0b732f2986d73031d
songhyeonpk <ehftozl1997@gmail.com>
2023-09-18 17:35:18 +0900

    [#95] refactor, feat : 게시글 첨부파일 삭제 로직 매개변수명 수정, 게시글 첨부파일 수정 로직 구현

--- src/main/java/com/sh/domain/board/service/BoardFileServiceImpl.java
+++ src/main/java/com/sh/domain/board/service/BoardFileServiceImpl.java
@@ -28,6 +28,7 @@ public class BoardFileServiceImpl implements BoardFileService {
     private final BoardRepository boardRepository;
     private final BoardAttachedFileRepository attachedFileRepository;
 
+    // 첨부파일 업로드 및 게시글 첨부파일 목록 반환
     @Override
     public List<BoardAttachedFile> uploadAttachedFiles(Board board, List<MultipartFile> files) {
         // 실제 경로에 파일 업로드
@@ -69,6 +70,7 @@ public class BoardFileServiceImpl implements BoardFileService {
                             .map(
                                     file ->
                                             BoardFileResponseDto.of(
+                                                    file.getId(),
                                                     file.getStoreFileName(),
                                                     file.getOriginalFileName(),
                                                     file.getFilePath(),
@@ -79,12 +81,44 @@ public class BoardFileServiceImpl implements BoardFileService {
         return result;
     }
 
-    // 게시글 첨부파일목록 삭제
+    // 게시글 첨부파일 삭제
     @Override
-    public void deleteAttachedFiles(List<BoardAttachedFile> fileList) {
-        for (BoardAttachedFile file : fileList) {
+    public void deleteAttachedFiles(List<BoardAttachedFile> files) {
+        for (BoardAttachedFile file : files) {
             // 실제 경로에서 파일 삭제
             fileUtils.deleteFile(file.getFilePath());
+        }
+    }
+
+    // 게시글 첨부파일 수정
+    @Override
+    public void modifyAttachedFiles(Long boardId, List<MultipartFile> files, List<Long> deleteFileIds) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> BoardCustomException.BOARD_NOT_FOUND);
+
+        // 삭제할 첨부파일의 값이 있을 경우
+        if(deleteFileIds != null && !deleteFileIds.isEmpty()) {
+            // 삭제할 파일목록 조회
+            List<BoardAttachedFile> deleteFiles = attachedFileRepository.findAllById(deleteFileIds);
+            
+            // 게시글 첨부파일 목록에서 삭제
+            for(BoardAttachedFile file : deleteFiles) {
+                board.removeAttachedFile(file);
+            }
+
+            // 실제 경로에서 삭제
+            deleteAttachedFiles(deleteFiles);
+        }
+
+        // 새로 추가한 첨부파일이 있을 경우
+        if(files != null && !files.isEmpty()) {
+            // 실제 경로에 첨부파일 추가 및 추가된 목록 조회
+            List<BoardAttachedFile> addFiles = uploadAttachedFiles(board, files);
+
+            // 게시글 첨부파일 목록에 추가
+            for(BoardAttachedFile file : addFiles) {
+                board.addAttachedFile(file);
+            }
         }
     }
 }
