04925b8f02c26ada6886689976e0acdd8a2434bf
songhyeonpk <ehftozl1997@gmail.com>
2023-09-22 17:11:12 +0900

    [#99] add : OAuth Server에 API 요청 처리 인터페이스 생성

---
+++ src/main/java/com/sh/global/oauth/OAuthApiClient.java
@@ -0,0 +1,12 @@
+package com.sh.global.oauth;
+
+public interface OAuthApiClient {
+    // Client 타입 반환
+    OAuthProvider oAuthProvider();
+
+    // Authorization Code 를 기반으로 인증 API 요청 후 Access Token 값 획득
+    String requestAccessToken(OAuthLoginParams params);
+    
+    // Access Token 을 기반으로 해당 사용자의 정보 획득
+    OAuthInfoResponse requestOauthInfo(String accessToken);
+}
