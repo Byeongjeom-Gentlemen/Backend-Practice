a212464d549fcea3837d7508ced6e13121d503e2
songhyeonpk <ehftozl1997@gmail.com>
2023-09-22 17:20:01 +0900

    [#99] add, feat : 카카오 관련 구현체 생성 및 카카오 API 요청 로직 구현

---
+++ src/main/java/com/sh/global/oauth/kakao/KakaoTokens.java
@@ -0,0 +1,33 @@
+package com.sh.global.oauth.kakao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+// 카카오 로그인 토큰 요청 결과 값
+@Getter
+public class KakaoTokens {
+
+    // access token
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    // 토큰 종류
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    // refresh token  
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    // 토큰 만료 시간
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    // refresh token 만료 시간
+    @JsonProperty("refresh_token_expires_in")
+    private Integer refreshTokenExpiresIn;
+
+    // 제공 데이터 종류
+    @JsonProperty("scope")
+    private String scope;
+}
