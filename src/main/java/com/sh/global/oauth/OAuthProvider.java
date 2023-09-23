c86da828fc197d0f7da64a713ecb8192f9bb25a6
songhyeonpk <ehftozl1997@gmail.com>
2023-09-23 16:51:40 +0900

    [#99] refactor : parsing 메소드 수정

--- src/main/java/com/sh/global/oauth/OAuthProvider.java
+++ src/main/java/com/sh/global/oauth/OAuthProvider.java
@@ -1,5 +1,18 @@
 package com.sh.global.oauth;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.sh.global.exception.customexcpetion.OAuthCustomException;
+
+import java.util.stream.Stream;
+
 public enum OAuthProvider {
-    KAKAO
+    KAKAO;
+
+    @JsonCreator
+    public static OAuthProvider parsing(String requestValue) {
+        return Stream.of(OAuthProvider.values())
+                .filter(oAuthProvider -> oAuthProvider.toString().equals(requestValue.toUpperCase()))
+                .findFirst()
+                .orElseThrow(() -> OAuthCustomException.UNSUPPORTED_OAUTH_PROVIDER);
+    }
 }
