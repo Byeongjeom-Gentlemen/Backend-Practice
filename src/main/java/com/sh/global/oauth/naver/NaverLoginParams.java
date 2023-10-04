9683947079a84df5bf6f32519ce23962504e09fb
songhyeonpk <ehftozl1997@gmail.com>
2023-10-04 17:51:54 +0900

    [#104] add : 네이버 로그인 파라미터 구현체 추가

---
+++ src/main/java/com/sh/global/oauth/naver/NaverLoginParams.java
@@ -0,0 +1,30 @@
+package com.sh.global.oauth.naver;
+
+import com.sh.global.oauth.OAuthLoginParams;
+import com.sh.global.oauth.OAuthProvider;
+import lombok.Getter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+public class NaverLoginParams implements OAuthLoginParams {
+
+    @Getter
+    private String authorizationCode;
+
+    @Getter
+    private String state;
+
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.NAVER;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        body.add("state", state);
+
+        return body;
+    }
+}
