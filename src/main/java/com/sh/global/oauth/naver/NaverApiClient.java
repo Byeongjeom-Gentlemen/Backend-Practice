285853933260c48e329c66a7d34c96e583223b10
songhyeonpk <ehftozl1997@gmail.com>
2023-10-04 17:54:18 +0900

    [#104] feat : 네이버 api 요청 구현체 추가, 네이버 토큰 요청 로직 및 네이버 회원 정보 요청 로직 구현

---
+++ src/main/java/com/sh/global/oauth/naver/NaverApiClient.java
@@ -0,0 +1,79 @@
+package com.sh.global.oauth.naver;
+
+import com.sh.global.oauth.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
+public class NaverApiClient implements OAuthApiClient {
+
+    public static final String GRANT_TYPE = "authorization_code";
+
+    @Value("${oauth.naver.request_url.auth}")
+    private String authUrl;
+
+    @Value("${oauth.naver.request_url.api}")
+    private String apiUrl;
+
+    @Value("${oauth.naver.client_id}")
+    private String clientId;
+
+    @Value("${oauth.naver.client_secret}")
+    private String clientSecret;
+
+    private final RestTemplate restTemplate;
+
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.NAVER;
+    }
+
+    // access token 발급 요청
+    @Override
+    public String requestAccessToken(OAuthLoginParams params) {
+        String url = authUrl + "/oauth2.0/token";
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> body = params.makeBody();
+        body.add("grant_type", GRANT_TYPE);
+        body.add("client_id", clientId);
+        body.add("client_secret", clientSecret);
+
+        HttpEntity<?> request = new HttpEntity<>(body, httpHeaders);
+        NaverTokens response = restTemplate.postForObject(url, request, NaverTokens.class);
+
+        assert response != null;
+        return response.getAccessToken();
+    }
+
+    // access token 으로 사용자 정보 요청
+    @Override
+    public OAuthInfoResponse requestOauthInfo(String accessToken) {
+        String url = apiUrl + "/v1/nid/me";
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        httpHeaders.set("Authorization", "Bearer " + accessToken);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+
+        HttpEntity<?> request = new HttpEntity<>(body, httpHeaders);
+
+        return restTemplate.postForObject(url, request, NaverInfoResponse.class);
+    }
+
+    @Override
+    public OAuthLogoutResponse requestOauthLogout(Long oauthId) {
+        return null;
+    }
+}
