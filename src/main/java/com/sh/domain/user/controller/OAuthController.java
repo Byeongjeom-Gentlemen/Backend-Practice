60c308a05484bb92dfa417d65cf90fd1da38d41d
songhyeonpk <ehftozl1997@gmail.com>
2023-10-06 17:08:44 +0900

    [#108] add : swagger 관련 코드 추가

--- src/main/java/com/sh/domain/user/controller/OAuthController.java
+++ src/main/java/com/sh/domain/user/controller/OAuthController.java
@@ -3,36 +3,46 @@ package com.sh.domain.user.controller;
 import com.sh.domain.user.dto.request.OAuthSignupRequestDto;
 import com.sh.domain.user.dto.response.OAuthLoginResponseDto;
 import com.sh.domain.user.service.OAuthService;
+import com.sh.global.aop.DisableSwaggerSecurity;
 import com.sh.global.aop.TokenValueRequired;
 import com.sh.global.oauth.kakao.KakaoLoginParams;
 import com.sh.global.oauth.naver.NaverLoginParams;
 import com.sh.global.util.jwt.TokenDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "OAuth", description = "OAuth API")
 public class OAuthController {
 
     private final OAuthService oAuthService;
 
     // 카카오 로그인 요청
-    @PostMapping("/api/v1/oauth/kakao/login")
+    @Operation(summary = "카카오 로그인 API", description = "카카오 로그인 API 입니다. 카카오 서버에서 발급받은 인가코드 값을 필요로 합니다.")
+    @DisableSwaggerSecurity
+    @PostMapping("/api/v1/oauth/kakao")
     @ResponseStatus(HttpStatus.OK)
     public OAuthLoginResponseDto kakaoLogin(@RequestBody KakaoLoginParams params) {
         return oAuthService.oauthLogin(params);
     }
 
     // 네이버 로그인 요청
-    @PostMapping("/api/v1/oauth/naver/login")
+    @Operation(summary = "네이버 로그인 API", description = "네이버 로그인 API 입니다. 네이버 서버에서 발급받은 인가코드 값과 상태 값을 필요로 합니다.")
+    @DisableSwaggerSecurity
+    @PostMapping("/api/v1/oauth/naver")
     @ResponseStatus(HttpStatus.OK)
     public OAuthLoginResponseDto naverLogin(@RequestBody NaverLoginParams params) {
         return oAuthService.oauthLogin(params);
     }
 
     // OAuth 회원가입
-    @PostMapping("/api/v1/oauth/join/{oauthProvider}")
+    @Operation(summary = "OAuth 회원가입 API", description = "OAuth 회원의 가입 API 입니다. 각 OAuth 서비스의 회원 고유번호와 닉네임을 파라미터 값으로 받습니다.")
+    @DisableSwaggerSecurity
+    @PostMapping("/api/v1/oauth/{oauthProvider}/join")
     @ResponseStatus(HttpStatus.CREATED)
     public Long oAuthJoin(
             @PathVariable String oauthProvider, @RequestBody OAuthSignupRequestDto signupRequest) {
